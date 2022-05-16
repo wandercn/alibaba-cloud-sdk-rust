74280972602b3af94ba1b1a65ba2cc5d6310c954
wander <wander@ffactory.org>
2022-05-16 14:03:34 +0800

    增加ShaHmac1算法函数

---
+++ src/sdk/auth/singers/algorithms.rs
@@ -0,0 +1,11 @@
+#![allow(unused)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+use base64;
+use ring::hmac;
+pub fn ShaHmac1(source: &str, secret: &str) -> String {
+    let key = hmac::Key::new(hmac::HMAC_SHA1_FOR_LEGACY_USE_ONLY, &secret.as_bytes());
+    let signedBytes = hmac::sign(&key, source.as_bytes());
+    base64::encode(signedBytes)
+}
