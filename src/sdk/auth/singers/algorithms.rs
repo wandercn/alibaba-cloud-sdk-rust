f69a3d7c1dd6141342bb3298528d0fc884316b2c
wander <wander@rustpub.com>
2025-04-29 23:29:08 +0800

    Update algorithms.rs

--- src/sdk/auth/singers/algorithms.rs
+++ src/sdk/auth/singers/algorithms.rs
@@ -2,11 +2,24 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
-use base64::{engine::general_purpose, Engine as _};
+use base64::{engine::general_purpose, DecodeError, Engine as _};
 use ring::hmac;
 pub fn ShaHmac1(source: &str, secret: &str) -> String {
     let key = hmac::Key::new(hmac::HMAC_SHA1_FOR_LEGACY_USE_ONLY, secret.as_bytes());
     let signedBytes = hmac::sign(&key, source.as_bytes());
-    // base64::encode(signedBytes)
-    general_purpose::STANDARD.encode(signedBytes)
+    base64_encode(signedBytes)
+}
+
+pub fn base64_encode<T>(input: T) -> String
+where
+    T: AsRef<[u8]>,
+{
+    general_purpose::STANDARD.encode(input)
+}
+
+pub fn base64_decode<T>(input: T) -> Result<Vec<u8>, DecodeError>
+where
+    T: AsRef<[u8]>,
+{
+    general_purpose::STANDARD.decode(input)
 }
